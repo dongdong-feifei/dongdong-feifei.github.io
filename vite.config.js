283d24c9e531f6024f4460ba495326e5fdd62f31
“dongdong-feifei <134391710+dongdong-feifei@users.noreply.github.com>
2024-10-16 21:32:00 +0800

    fix:404配置remove hash

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,45 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',  // 设置为相对路径
-    plugins: [
-        
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
-    resolve: {
-        alias: {
-          'vue': 'vue/dist/vue.esm-bundler.js'  // 这里指定 Vue 的解析路径
-        }
-      },
-    css: {
-        preprocessorOptions: {
-            less: {
-                additionalData: `@import "${path.resolve(__dirname, 'src/assets/style/variable.less')}";`,
-                javascriptEnabled: true,
-            }
-        },
+  base: "./", // 设置为相对路径
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js", // 这里指定 Vue 的解析路径
     },
-
-})
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `@import "${path.resolve(
+          __dirname,
+          "src/assets/style/variable.less"
+        )}";`,
+        javascriptEnabled: true,
+      },
+    },
+  },
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
+});
